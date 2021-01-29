d3a188de8cb36d9d70edf1835d2d662fee75e044
omerasikoglu <59120165+omerasikoglu@users.noreply.github.com>
2021-01-29 21:35:34 +0300

    level

---
+++ SOScripts/Level/LevelSO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelSO", menuName = "LevelDesign/Level")]
+
+public class LevelSO : ScriptableObject
+{
+    [Header("[Level Details]")]
+    public int sceneNumber;
+    public List<StarTypeSO> starTypesList;
+}
---
+++ SOScripts/Level/StarTypeSO.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "StarTypeSO", menuName = "LevelDesign/StarType")]
+
+public class StarTypeSO : ScriptableObject
+{
+    public string nameString;
+    public Sprite notAchievedSprite;
+    public Sprite achievedSprite;
+    public string tooltipString;
+    public bool isAchieved=false;
+}

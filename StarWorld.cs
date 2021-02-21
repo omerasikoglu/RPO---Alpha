95aadfc589a84ddfd97a16fd3d8e8a5a39a8497e
omerasikoglu <59120165+omerasikoglu@users.noreply.github.com>
2021-02-21 09:04:26 +0300

    Stat and Star Config

--- StarWorld.cs
+++ StarWorld.cs
@@ -5,36 +5,71 @@ using UnityEngine;
 
 public class StarWorld : MonoBehaviour
 {
-    public event System.EventHandler OnStarAchieved; //level içi
+    public static StarWorld SpawnStarWorld(Vector2 position, ResourceTypeSO starType)
+    {
+        Transform transform = Instantiate(WorldAssets.Instance.pfStarWorld, position, Quaternion.identity);
 
-    [SerializeField] ResourceData resourceData;
-    //[SerializeField] bool isAchieved = false;
+        StarWorld starWorld = transform.GetComponent<StarWorld>();
+
+        starWorld.SetStarType(starType);
+
+        return starWorld;
+    }
+
+    private ResourceTypeSO starType;
     private SpriteRenderer spriteRenderer;
-    [SerializeField] private LevelSO currentLevel;
-
-
     private void Awake()
     {
-
         spriteRenderer = GetComponent<SpriteRenderer>();
-        spriteRenderer.sprite = resourceData.resourceType.sprite;
+    }
 
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void SetStarType(ResourceTypeSO starType)
     {
-        if (collision.transform.CompareTag("Player"))
-        {
-            int index = 0;
-            foreach (ResourceTypeSO starType in currentLevel.starTypesList)
-            {
-                if (starType == resourceData.resourceType)
-                {
-                    StarManager.Instance.SetStarAchieved(index, resourceData.resourceType, resourceData.amount);
-                }
-                index++;
-            }
-            Destroy(gameObject);
-        }
+        this.starType = starType;
+        spriteRenderer.sprite = starType.sprite;
+        spriteRenderer.color = starType.GetColor();
     }
+    public ResourceTypeSO GetStarType()
+    {
+        return starType;
+    }
+
+    public void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+
+    //public event System.EventHandler OnStarAchieved; //level içi
+
+    //[SerializeField] ResourceData resourceData;
+    ////[SerializeField] bool isAchieved = false;
+    //private SpriteRenderer spriteRenderer;
+    //[SerializeField] private LevelSO currentLevel;
+
+
+    //private void Awake()
+    //{
+
+    //    spriteRenderer = GetComponent<SpriteRenderer>();
+    //    spriteRenderer.sprite = resourceData.resourceType.sprite;
+
+    //}
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.transform.CompareTag("Player"))
+    //    {
+    //        int index = 0;
+    //        foreach (ResourceTypeSO starType in currentLevel.starTypesList)
+    //        {
+    //            if (starType == resourceData.resourceType)
+    //            {
+    //                StarManager.Instance.SetStarAchieved(index, resourceData.resourceType, resourceData.amount);
+    //            }
+    //            index++;
+    //        }
+    //        Destroy(gameObject);
+    //    }
+    //}
+
 
 }
---
+++ StarWorldSpawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarWorldSpawner : MonoBehaviour
+{
+    public ResourceTypeSO starType;
+
+    private void Awake()
+    {
+        StarWorld.SpawnStarWorld(transform.position, starType);
+        Destroy(gameObject);
+    }
+}

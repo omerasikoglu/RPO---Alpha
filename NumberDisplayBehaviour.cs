066a04826ff89dbe4eb23834a9a52f0a5701a693
omerasikoglu <59120165+omerasikoglu@users.noreply.github.com>
2021-06-21 00:08:03 +0300

    Object Pooling
    Hit Numbers Pooling and VFX stuff

---
+++ NumberDisplayBehaviour.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberDisplayBehaviour : MonoBehaviour
+{
+    [Header("Animation")]
+    public Animation numberAnimation;
+
+    [Header("Display")]
+    public TextMeshPro textDisplay;
+}
